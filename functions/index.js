72c7f42b20e6abec914d4541a14cfb3c160d7e77
Carlo Renzo Huaman Torres <cr.htorres@gmail.com>
2017-08-22 00:36:25 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,33 @@
 // Architecture WebHook Actions
-
 'use strict';
 
 process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').ApiAiApp;
 const functions = require('firebase-functions');
 
-// [START YourAction]
-exports.yourAction = functions.https.onRequest((request, response) => {
+// [START MyAction]
+exports.architectureHelps = functions.https.onRequest((request, response) => {
   const app = new App({request, response});
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
 
   // Fulfill action business logic
-  function responseHandler (app) {
-    // Complete your fulfillment logic and send a response
-    app.tell('Hello, World!');
+  function calculateDistanceHandler (app) {
+   //let distance = DEFAULT_DISTANCE;
+   let distanceCategory = app.getArgument('architecture-category');
+
+   if(distanceCategory === 'column'){
+     app.tell('The correct distance between two columns is two sishos -> By Tohure');
+   } else if(distanceCategory === 'beam'){
+     app.tell('Distance between beems is Unknown -> By Tohure');
+   } else {
+     app.tell('I dont found the correct Response -> By Tohure');
+   }
+
+   const actionMap = new Map();
+   actionMap.set('calculate.distance', calculateDistanceHandler);
+
+   app.handleRequest(actionMap);
   }
-
-  const actionMap = new Map();
-  actionMap.set('input.welcome', responseHandler);
-
-  app.handleRequest(actionMap);
 });
-// [END YourAction]
+// [END MyAction]
