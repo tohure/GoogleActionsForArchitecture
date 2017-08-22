2a88f253329fde486d840c3a44395b19c52d3eb0
Carlo Renzo Huaman Torres <cr.htorres@gmail.com>
2017-08-22 18:01:19 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,27 +7,67 @@ const functions = require('firebase-functions');
 
 // [START MyAction]
 exports.architectureHelps = functions.https.onRequest((request, response) => {
-  const app = new App({request, response});
-  console.log('Request headers: ' + JSON.stringify(request.headers));
-  console.log('Request body: ' + JSON.stringify(request.body));
+    const app = new App({ request, response });
+    console.log('Request headers: ' + JSON.stringify(request.headers));
+    console.log('Request body: ' + JSON.stringify(request.body));
 
-  // Fulfill action business logic
-  function calculateDistanceHandler (app) {
-   //let distance = DEFAULT_DISTANCE;
-   let distanceCategory = app.getArgument('architecture-category');
+    //My Actions
+    const Actions = {
+        CALCULATE_DISTANCE: 'calculate.distance'
+    };
 
-   if(distanceCategory === 'column'){
-     app.tell('The correct distance between two columns is two sishos -> By Tohure');
-   } else if(distanceCategory === 'beam'){
-     app.tell('Distance between beems is Unknown -> By Tohure');
-   } else {
-     app.tell('I dont found the correct Response -> By Tohure');
-   }
+    //My Parameters
+    const Parameters = {
+        ARCHITECT_CATEGORY: 'architecture-category',
+        NUMBER_CATEGORY: 'number',
+    };
 
-   const actionMap = new Map();
-   actionMap.set('calculate.distance', calculateDistanceHandler);
+    const screenOutput = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
 
-   app.handleRequest(actionMap);
-  }
+    // Fulfill action business logic
+    function calculateDistanceHandler(app) {
+
+        let architectureCategory = app.getArgument(Parameters.ARCHITECT_CATEGORY);
+        let numberCategory = app.getArgument(Parameters.NUMBER_CATEGORY);
+
+        if (architectureCategory == 'column' || architectureCategory == 'columns') {
+
+            sendResponse({
+                title: "Columns",
+                msg: "I dont know about colums. but in another hand, what do you prefer know? Columns or beams?"
+            }, {
+                    src: "https://t4.ftcdn.net/jpg/00/82/94/69/240_F_82946972_wcTRSPuo0byFk4S3paw5RaBgtvAr5ZOr.jpg",
+                    title: "Greek Columns"
+                }, ['Columns', 'Beams']);
+
+        } else if (architectureCategory == 'beam' || architectureCategory == 'beams') {
+
+            sendResponse({
+                title: "Beams",
+                msg: "Between Beams is three sishos. Now you prefer continue with Columns or beams?"
+            }, {
+                    src: "http://www.myejen.com/decor/2014/05/Cozy-Dining-Area-Design-in-White-Cycladic-House-with-Beams.jpg",
+                    title: "Greek Beams"
+                }, ['Beams', 'Columns']);
+
+        } else {
+            app.tell('I dont found the correct Response -->' + architectureCategory + ' --> By Tohure');
+        }
+    }
+
+    function sendResponse(category, img, suggestions) {
+        if (screenOutput) {
+            app.ask(app.buildRichResponse()
+                .addSimpleResponse(category.msg)
+                .addBasicCard(app.buildBasicCard(category.title).setImage(img.src, img.title))
+                .addSuggestions(suggestions));
+        } else {
+            app.ask(category.msg);
+        }
+    }
+
+    const actionMap = new Map();
+    actionMap.set('calculate.distance', calculateDistanceHandler);
+    app.handleRequest(actionMap);
 });
 // [END MyAction]
